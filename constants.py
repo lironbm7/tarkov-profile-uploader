5294706069cccb7be70e16008ebbac884b449d21
Liron Biam <lironbm27@gmail.com>
2024-10-05 09:04:57 -0700

    constants and placeholders for prerequisites

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Local path to the 'profiles' directory within the SPT directory
+PROFILES_DIRECTORY = 'C:/SPT/user/profiles'
+
+# Google Drive API Authentication Configuration
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+CREDENTIALS_FILE_PATH = 'path/to/credentials.json'
+TOKEN_PICKLE = 'token.pickle'
+
+# Google Drive folder name for profiles upload
+PROFILES_DIRECTORY_IN_GOOGLE_DRIVE = 'spt-profiles'
